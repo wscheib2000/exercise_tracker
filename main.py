0206274aaabec9251cf43f1a04e22683d87664de
wscheib2000 <william.scheib@gatech.edu>
2024-03-28 15:26:06 -0400

    Generalized first post request and added Sheety post request to add row(s)

--- main.py
+++ main.py
@@ -16,9 +16,29 @@ headers = {
     'x-app-key': API_KEY
 }
 params = {
-    'query': 'Ran for 2 miles'
+    'query': input('What exercise did you do? ')
 }
 
 response = requests.post(url=nutritionix_stem+nlp_exercise_endpoint, json=params, headers=headers)
 response.raise_for_status()
 data = response.json()
+
+sheety_stem = 'https://api.sheety.co'
+sheety_endpoint = '/d9f02081f9a01191e5a5c45c80a67854/myWorkouts (pythonProject)/workouts'
+headers = {
+    'Authorization': SHEETY_TOKEN,
+    'Content-Type': 'application/json'
+}
+for exercise in data['exercises']:
+    row = {
+        'workout': {
+            'date': datetime.now().strftime('%d/%m/%Y'),
+            'time': datetime.now().strftime('%H:%M:%S'),
+            'exercise': exercise['name'].title(),
+            'duration': exercise['duration_min'],
+            'calories': exercise['nf_calories']
+        }
+    }
+
+    response = requests.post(url=sheety_stem+sheety_endpoint, json=row, headers=headers)
+    response.raise_for_status()
