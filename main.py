1041ebac54333490f0d6bd9582d9a2d92707319c
wscheib2000 <william.scheib@gatech.edu>
2024-03-28 15:25:06 -0400

    Post request to Nutritionix

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from dotenv import load_dotenv
 import os
+import requests
+from datetime import datetime
 
 load_dotenv()
 
 APP_ID = os.environ.get('APP_ID')
 API_KEY = os.environ.get('API_KEY')
+SHEETY_TOKEN = os.environ.get('SHEETY_TOKEN')
+
+nutritionix_stem = 'https://trackapi.nutritionix.com'
+nlp_exercise_endpoint = '/v2/natural/exercise'
+headers = {
+    'x-app-id': APP_ID,
+    'x-app-key': API_KEY
+}
+params = {
+    'query': 'Ran for 2 miles'
+}
+
+response = requests.post(url=nutritionix_stem+nlp_exercise_endpoint, json=params, headers=headers)
+response.raise_for_status()
+data = response.json()
